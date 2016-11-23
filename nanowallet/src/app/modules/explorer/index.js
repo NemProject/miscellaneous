5320614affe9197c61e69a6b93cb9ad2285694de
Quantum_Mechanics <Kraken_Labs@outlook.com>
2016-11-23 20:27:20 +0100

    Update explorer module index

--- nanowallet/src/app/modules/explorer/index.js
+++ nanowallet/src/app/modules/explorer/index.js
@@ -22,4 +22,30 @@ explorerApostillesModule.config(ExplorerApostillesConfig);
 import ExplorerApostillesCtrl from './apostilles/explorerApostilles.controller';
 explorerApostillesModule.controller('ExplorerApostillesCtrl', ExplorerApostillesCtrl);
 
+// Create the module where our functionality can attach to
+let explorerNamespacesMosaicsModule = angular.module('app.explorerNamespacesMosaics', []);
+
+// Include our UI-Router config settings
+import ExplorerNamespacesMosaicsConfig from './namespaces-mosaics/namespaces-mosaics.config';
+explorerNamespacesMosaicsModule.config(ExplorerNamespacesMosaicsConfig);
+
+// Controllers
+import ExplorerNamespacesMosaicsCtrl from './namespaces-mosaics/namespaces-mosaics.controller';
+explorerNamespacesMosaicsModule.controller('ExplorerNamespacesMosaicsCtrl', ExplorerNamespacesMosaicsCtrl);
+
+// Create the module where our functionality can attach to
+let accountsExplorerModule = angular.module('app.accountsExplorer', []);
+
+// Include our UI-Router config settings
+import AccountsExplorerConfig from './accounts/accountsExplorer.config';
+accountsExplorerModule.config(AccountsExplorerConfig);
+
+// Controllers
+import AccountsExplorerCtrl from './accounts/accountsExplorer.controller';
+accountsExplorerModule.controller('AccountsExplorerCtrl', AccountsExplorerCtrl);
+
+// Components
+import ExplorerNav from './layout/nav.component';
+explorerHomeModule.component('explorerNav', ExplorerNav);
+
 export default explorerHomeModule;
