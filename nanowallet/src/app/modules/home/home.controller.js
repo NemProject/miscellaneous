0ddb62d9e934b77a8609562cfd416028ff906161
Quantum_Mechanics <Kraken_Labs@outlook.com>
2017-03-16 20:49:28 +0100

    Fix display of Namespaces owned by multisig

--- nanowallet/src/app/modules/home/home.controller.js
+++ nanowallet/src/app/modules/home/home.controller.js
@@ -4,6 +4,19 @@ class HomeCtrl {
 
         this.appName = AppConstants.appName;
 
+        // Detect recommended browsers
+        let isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+        let isSafari = /Safari/.test(navigator.userAgent) && /Apple Computer/.test(navigator.vendor);
+        let isFirefox = /Firefox/.test(navigator.userAgent);
+
+        // If no recommended browser used, open modal
+        if(!isChrome && !isSafari && !isFirefox) {
+        	$('#noSupportModal').modal({
+			  backdrop: 'static',
+			  keyboard: false
+			}); 
+        }
+
     }
 }
 
