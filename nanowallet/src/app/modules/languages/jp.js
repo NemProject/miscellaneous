dd6d91de1a5d184dd8656994da54cce7e2e7ab83
MIZUNASHI, Rin <mizunashi.rin@gmail.com>
2017-05-06 05:20:58 +0900

    insert comment

--- nanowallet/src/app/modules/languages/jp.js
+++ nanowallet/src/app/modules/languages/jp.js
@@ -143,8 +143,8 @@ function JapaneseProvider($translateProvider) {
       GENERAL_CURRENCY: '通貨',
       GENERAL_EDIT: '編集',
       GENERAL_SEND_XEM: 'XEMの送金',
-      GENERAL_ACTIVE: 'Active',
-      GENERAL_INACTIVE: 'Inactive',
+      GENERAL_ACTIVE: 'Active', //Remote が英語対応したら対応予定
+      GENERAL_INACTIVE: 'Inactive', //同上
         
       // HOME MODULE
       HOME_UNSUPPORTED_BROWSER: '申し訳ありません。このブラウザでは安全に NanoWallet を使用する事ができません。',
