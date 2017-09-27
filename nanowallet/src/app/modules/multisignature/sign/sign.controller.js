4e9bda7651716542273605375682b6c63a01599b
Quantum_Mechanics <Kraken_Labs@outlook.com>
2017-09-27 18:33:45 +0200

    Improve sign multisig transactions module

--- nanowallet/src/app/modules/multisignature/sign/sign.controller.js
+++ nanowallet/src/app/modules/multisignature/sign/sign.controller.js
@@ -1,47 +1,40 @@
-import helpers from '../../../utils/helpers';
-import Serialization from '../../../utils/Serialization';
-import convert from '../../../utils/convert';
+import nem from 'nem-sdk';
 
 class SignMultisigCtrl {
-    constructor(Wallet, Alert, $location, DataBridge, NetworkRequests) {
+
+    /**
+     * Initialize dependencies and properties
+     *
+     * @params {services} - Angular services to inject
+     */
+    constructor(Wallet, Alert, $timeout) {
         'ngInject';
 
-        // Alert service
+        //// Module dependencies region ////
+
         this._Alert = Alert;
-        // $location to redirect
-        this._location = $location;
-        // Wallet service
         this._Wallet = Wallet;
-        // DataBridge service
-        this._DataBridge = DataBridge;
-        // Network requests service
-        this._NetworkRequests = NetworkRequests;
+        this._$timeout = $timeout;
 
-        // If no wallet show alert and redirect to home
-        if (!this._Wallet.current) {
-            this._Alert.noWalletLoaded();
-            this._location.path('/');
-            return;
-        }
+        //// End dependencies region ////
 
-        /**
-         * Default properties 
-         */
+        //// Module properties region ////
 
-         // Store unconfirmed txes
-         this.unconfirmed = [];
+        // Store unconfirmed txes
+        this.unconfirmed = [];
 
         // Unconfirmed txes pagination properties
         this.currentPage = 0;
         this.pageSize = 5;
-        this.numberOfPages = function() {
-            return Math.ceil(this.unconfirmed.length / this.pageSize);
-        }
+
+        //// End properties region ////
 
         // Get unconfirmed transactions
         this.getUnconfirmedTransactions();
 
     }
+
+    //// Module methods region ////
 
     /**
      * Fetch unconfirmed transactions for the current account
@@ -50,32 +43,29 @@ class SignMultisigCtrl {
         // Reset to initial page
         this.currentPage = 0;
         // 
-        this._NetworkRequests.getUnconfirmedTxes(helpers.getHostname(this._Wallet.node), this._Wallet.currentAccount.address).then((res) => {
-            this.unconfirmed = res.data;
-            for (let i = 0; i < res.data.length; i++) {
-                this.unconfirmed[i].meta.innerHash = {
-                    "data": res.data[i].meta.data
+        nem.com.requests.account.transactions.unconfirmed(this._Wallet.node, this._Wallet.currentAccount.address).then((res) => {
+            this._$timeout(() => {
+                this.unconfirmed = res.data;
+                for (let i = 0; i < res.data.length; i++) {
+                    this.unconfirmed[i].meta.innerHash = {
+                        "data": res.data[i].meta.data
+                    }
+                    this.unconfirmed[i].meta.height = 9007199254740991;
                 }
-                // Not showing the good hash for some reason, but unecessary
-                /*let byteArray = Serialization.serializeTransaction(this.unconfirmed[i].transaction);
-                let txHash = CryptoJS.SHA3(convert.ua2words(byteArray, byteArray.length), {
-                    outputLength: 256
-                });
-                this.unconfirmed[i].meta.hash = {
-                    "data": txHash.toString(CryptoJS.enc.Hex)
-                }*/
-                this.unconfirmed[i].meta.height = 9007199254740991;
-            }
+            });
         },
         (err) => {
-            if(err.status === -1) {
-                this._Alert.connectionError();
-            } else {
-                this._Alert.errorGetTransactions(err.data.message);
-            }
+            this._$timeout(() => {
+                if(err.code < 0) {
+                    this._Alert.connectionError();
+                }  else {
+                    this._Alert.errorGetTransactions(err.data.message);
+                }
+            });
         });
     }
 
+    //// End methods region ////
 
 }
 
