cd196db2ec76ef68d05ddcef8622da1739a45284
Quantum_Mechanics <Kraken_Labs@outlook.com>
2017-09-27 18:28:23 +0200

    Improve Changelly module

--- nanowallet/src/app/modules/changelly/changelly.controller.js
+++ nanowallet/src/app/modules/changelly/changelly.controller.js
@@ -1,21 +1,20 @@
 class ChangellyCtrl {
-    constructor($location, Wallet, Alert) {
+
+    /**
+     * Initialize dependencies and properties
+     *
+     * @params {services} - Angular services to inject
+     */
+    constructor(Wallet) {
         'ngInject';
 
-        // Alert service
-        this._Alert = Alert;
-        // $location to redirect
-        this._location = $location;
-        // Wallet service
+        //// Module dependencies region ////
+
         this._Wallet = Wallet;
 
-        // If no wallet show alert and redirect to home
-        if (!this._Wallet.current) {
-            this._Alert.noWalletLoaded();
-            this._location.path('/');
-            return;
-        }
+        //// End dependencies region ////
 
+        // Load the Changelly widget in view
         document.getElementById('changwidget').innerHTML = '<iframe src="https://changelly.com/widget/v1?auth=merchant&from=BTC&to=XEM&merchant_id=9bad2685d41a&address='+ this._Wallet.currentAccount.address +'&amount=1&ref_id=9bad2685d41a&color=454545" width="100%" height="500" class="changelly" scrolling="no" style="overflow-y: hidden; border: none" > Can\'t load widget </iframe>'; 
 
     }
