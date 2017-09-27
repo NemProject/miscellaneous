87b8b7cb84745b962f87caf1cce4f99d928c592e
Quantum_Mechanics <Kraken_Labs@outlook.com>
2017-09-27 18:30:18 +0200

    Improve edit mosaics module

--- nanowallet/src/app/modules/mosaics/edit/editMosaic.controller.js
+++ nanowallet/src/app/modules/mosaics/edit/editMosaic.controller.js
@@ -1,166 +1,144 @@
-import CryptoHelpers from '../../../utils/CryptoHelpers';
-import Sinks from '../../../utils/sinks';
+import nem from 'nem-sdk';
 
 class editMosaicCtrl {
-    constructor($location, Wallet, Alert, Transactions, DataBridge, $localStorage) {
+
+    /**
+     * Initialize dependencies and properties
+     *
+     * @params {services} - Angular services to inject
+     */
+    constructor(Wallet, Alert, DataStore, $timeout) {
         'ngInject';
 
-        // Alert service
-        this._Alert = Alert;
-        // $location to redirect
-        this._location = $location;
-        // Wallet service
-        this._Wallet = Wallet;
-        // Transactions service
-        this._Transactions = Transactions;
-        // DataBridge service
-        this._DataBridge = DataBridge;
-        //Local storage
-        this._storage = $localStorage;
+        //// Module dependencies region ////
 
-        // If no wallet show alert and redirect to home
-        if (!this._Wallet.current) {
-            this._Alert.noWalletLoaded();
-            this._location.path('/');
-            return;
-        }
+        this._Alert = Alert;
+        this._Wallet = Wallet;
+        this._DataStore = DataStore;
+        this._$timeout = $timeout;
+
+        //// End dependencies region ////
  
-        /**
-         *  Default mosaic supply change transaction properties  
-         */
-        this.formData = {};
-        this.formData.mosaic = '';
-        this.formData.supplyType = 1;
-        this.formData.delta = 0;
-        this.formData.fee = 0;
-        // Multisig data
-        this.formData.innerFee = 0;
-        this.formData.isMultisig = false;
-        this.formData.multisigAccount = this._DataBridge.accountData.meta.cosignatoryOf.length == 0 ? '' : this._DataBridge.accountData.meta.cosignatoryOf[0];
+        //// Module properties region ////
+
+        // Form is a mosaic supply change transaction object
+        this.formData = nem.model.objects.get("mosaicSupplyChangeTransaction");
+        
+        // Set first multisig account if any
+        this.formData.multisigAccount = this._DataStore.account.metaData.meta.cosignatoryOf.length == 0 ? '' : this._DataStore.account.metaData.meta.cosignatoryOf[0];
 
         // Default current account address (used in view to get mosaic owned by account)
-        this.currentAccount = this._Wallet.currentAccount.address;
+        this.mosaicOwned = this._DataStore.mosaic.ownedBy[this._Wallet.currentAccount.address];
+
         // Mosaics owned names for current account
         this.currentAccountMosaicNames = '';
+
         // Selected mosaic from view
         this.selectedMosaic = '';
-
-        this.contacts = [];
-
-        if(undefined !== this._storage.contacts && undefined !== this._storage.contacts[this._Wallet.currentAccount.address] && this._storage.contacts[this._Wallet.currentAccount.address].length) {
-            this.contacts = this._storage.contacts[this._Wallet.currentAccount.address]
-        }
 
         // Needed to prevent user to click twice on send when already processing
         this.okPressed = false;
 
         // Object to contain our password & private key data.
-        this.common = {
-            'password': '',
-            'privateKey': '',
-        };
+        this.common = nem.model.objects.get("common");
+
+        // Store the prepared transaction
+        this.preparedTransaction = {};
+
+        //// End properties region ////
 
         // Get mosaics for current account
         this.updateCurrentAccountMosaics();
 
-        this.updateFees();
+        // Update the fee in view
+        this.prepareTransaction();
     }
 
+    //// Module methods region ////
+
     /**
-     * updateMosaic() Update mosaic data for selected mosaic
+     * Update mosaic data for selected mosaic
      *
      * @note: Used in view (ng-update) on selected mosaic changes
      */
     updateMosaic() {
-        this.formData.mosaic = this._DataBridge.mosaicOwned[this.currentAccount][this.selectedMosaic].mosaicId;
+        this.formData.mosaic = this.mosaicOwned[this.selectedMosaic].mosaicId;
     }
 
     /**
-     * updateCurrentAccountMosaics() Get current account mosaics owned names
+     * Get current account mosaics owned names
      *
      * @note: Used in view (ng-update) on multisig changes
      */
     updateCurrentAccountMosaics() {
-        //Fix this.formData.multisigAccount error on logout
-        if (null === this.formData.multisigAccount) {
-            return;
-        }
-        if (this.formData.isMultisig) {
-            this.currentAccount = this.formData.multisigAccount.address;
-        } else {
-            this.currentAccount = this._Wallet.currentAccount.address;
-        }
+        // Get current account
+        let acct = this.formData.isMultisig ? this.formData.multisigAccount.address : this._Wallet.currentAccount.address;
         // Set current account mosaics names if mosaicOwned is not undefined
-        if (undefined !== this._DataBridge.mosaicOwned[this.currentAccount]) {
-            this.currentAccountMosaicNames = Object.keys(this._DataBridge.mosaicOwned[this.currentAccount]).sort();
+        if (undefined !== this._DataStore.mosaic.ownedBy[acct]) {
+            this.currentAccountMosaicNames = Object.keys(this._DataStore.mosaic.ownedBy[acct]).sort();
+            this.mosaicOwned = this._DataStore.mosaic.ownedBy[acct];
         } else {
             this.currentAccountMosaicNames = ['nem:xem'];
+            this.mosaicOwned = {};
         }
         // Default mosaic selected
         this.selectedMosaic = "nem:xem";
-
-        this.updateFees();
+        this.prepareTransaction();
     }
 
     /**
-     * updateFees() Update transaction fee
+     * Prepare the transaction
      */
-    updateFees() {
-        let entity = this._Transactions.prepareMosaicSupply(this.common, this.formData);
-        if (this.formData.isMultisig) {
-            this.formData.innerFee = entity.otherTrans.fee;
-        } else {
-            this.formData.innerFee = 0;
-        }
-        this.formData.fee = entity.fee;
+    prepareTransaction() {
+        let entity = nem.model.transactions.prepare("mosaicSupplyChangeTransaction")(this.common, this.formData, this._Wallet.network);
+        this.preparedTransaction = entity;
+        return entity;
     }
 
     /**
-     * send() Build and broadcast the transaction to the network
+     * Reset data
+     */
+    resetData() {
+        this.formData = nem.model.objects.get("mosaicSupplyChangeTransaction");
+        this.common = nem.model.objects.get("common");
+        this.preparedTransaction = {};
+        this.prepareTransaction();
+    }
+
+    /**
+     * Prepare and broadcast the transaction to the network
      */
     send() {
         // Disable send button;
         this.okPressed = true;
 
-        // Decrypt/generate private key and check it. Returned private key is contained into this.common
-        if (!CryptoHelpers.passwordToPrivatekeyClear(this.common, this._Wallet.currentAccount, this._Wallet.algo, true)) {
-            this._Alert.invalidPassword();
-            // Enable send button
-            this.okPressed = false;
-            return;
-        } else if (!CryptoHelpers.checkAddress(this.common.privateKey, this._Wallet.network, this._Wallet.currentAccount.address)) {
-            this._Alert.invalidPassword();
-            // Enable send button
-            this.okPressed = false;
-            return;
-        }
+        // Get account private key for preparation or return
+        if (!this._Wallet.decrypt(this.common)) return this.okPressed = false;
 
-        // Build the entity to serialize
-        let entity = this._Transactions.prepareMosaicSupply(this.common, this.formData);
-        // Construct transaction byte array, sign and broadcast it to the network
-        return this._Transactions.serializeAndAnnounceTransaction(entity, this.common).then((res) => {
-                // Check status
-                if (res.status === 200) {
-                    // If code >= 2, it's an error
-                    if (res.data.code >= 2) {
-                        this._Alert.transactionError(res.data.message);
-                    } else {
-                        this._Alert.transactionSuccess();
-                    }
-                }
+        // Prepare the transaction
+        let entity = this.prepareTransaction();
+
+        // Use wallet service to serialize and send
+        this._Wallet.transact(this.common, entity).then(() => {
+            this._$timeout(() => {
                 // Enable send button
                 this.okPressed = false;
-                // Delete private key in common
-                this.common.privateKey = '';
-            },
-            (err) => {
-                // Delete private key in common
-                this.common.privateKey = '';
-                // Enable send button
-                this.okPressed = false;
-                this._Alert.transactionError('Failed ' + err.data.error + " " + err.data.message);
+                // Reset form data
+                this.resetData();
+                return;
             });
+        }, () => {
+            this._$timeout(() => {
+                // Delete private key in common
+                this.common.privateKey = '';
+                // Enable send button
+                this.okPressed = false;
+                return;
+            });
+        });
     }
+
+    //// End methods region ////
 
 }
 
