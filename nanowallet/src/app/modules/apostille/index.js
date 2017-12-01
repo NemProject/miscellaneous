8d55745ce0e9cd7f45c853336198e7b3571c29ae
Quantum_Mechanics <Kraken_Labs@outlook.com>
2017-12-01 19:16:00 +0100

    Add basic apostille modules

---
+++ nanowallet/src/app/modules/apostille/index.js
@@ -0,0 +1,36 @@
+import angular from 'angular';
+
+// Create the module where our functionality can attach to
+let createApostilleModule = angular.module('app.createApostille', []);
+
+// Include our UI-Router config settings
+import CreateApostilleConfig from './create/createApostille.config';
+createApostilleModule.config(CreateApostilleConfig);
+
+// Controllers
+import CreateApostilleCtrl from './create/createApostille.controller';
+createApostilleModule.controller('CreateApostilleCtrl', CreateApostilleCtrl);
+
+// Create the module where our functionality can attach to
+let auditApostilleModule = angular.module('app.auditApostille', []);
+
+// Include our UI-Router config settings
+import AuditApostilleConfig from './audit/auditApostille.config';
+auditApostilleModule.config(AuditApostilleConfig);
+
+// Controllers
+import AuditApostilleCtrl from './audit/auditApostille.controller';
+auditApostilleModule.controller('AuditApostilleCtrl', AuditApostilleCtrl);
+
+// Create the module where our functionality can attach to
+let apostilleHistoryModule = angular.module('app.apostilleHistory', []);
+
+// Include our UI-Router config settings
+import ApostilleHistoryConfig from './history/apostilleHistory.config';
+apostilleHistoryModule.config(ApostilleHistoryConfig);
+
+// Controllers
+import ApostilleHistoryCtrl from './history/apostilleHistory.controller';
+apostilleHistoryModule.controller('ApostilleHistoryCtrl', ApostilleHistoryCtrl);
+
+export default createApostilleModule;
