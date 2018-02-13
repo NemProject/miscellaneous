71a4322798e9994f0353b595e667785714c932fd
Quantum_Mechanics <Kraken_Labs@outlook.com>
2018-02-13 21:03:56 +0100

    Add exchanges list

---
+++ nanowallet/src/app/utils/exchanges.js
@@ -0,0 +1,20 @@
+/**
+ * Exchanges requiring a message for deposits
+ */
+let data =  [{
+    name: "Poloniex",
+    address: "NBZMQO7ZPBYNBDUR7F75MAKA2S3DHDCIFG775N3D"
+}, {
+    name: "Bittrex",
+    address: "ND2JRPQIWXHKAA26INVGA7SREEUMX5QAI6VU7HNR"
+}, {
+    name: "Spectrocoin",
+    address: "NAMLQUBJNNJRV5Y47GNJ7VXB5OHJHYBLHOYF7RVR"
+}, {
+    name: "Zaif",
+    address: "NAGJG3QFWYZ37LMI7IQPSGQNYADGSJZGJRD2DIYA"
+}]
+
+module.exports = {
+    data
+}
