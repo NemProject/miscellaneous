8afd70c930b326b92263e4f3463b1505158eff3a
Quantum_Mechanics <Kraken_Labs@outlook.com>
2018-02-14 18:34:10 +0100

    Improve wallet check in state transitions

--- nanowallet/src/app/config/app.run.js
+++ nanowallet/src/app/config/app.run.js
@@ -1,6 +1,17 @@
 function AppRun(AppConstants, $rootScope, $timeout, Wallet, Alert, $transitions) {
     'ngInject';
 
+    const publicStates = [
+        "app.home",
+        "app.login",
+        "app.signup",
+        "app.faq",
+        "app.trezor",
+        "app.offlineTransactionHome",
+        "app.offlineTransactionCreate",
+        "app.offlineTransactionSend"
+    ];
+
      // Change page title based on state
     $transitions.onSuccess({ to: true }, (transition) => {
         $rootScope.setPageTitle(transition.router.globals.current.title);
@@ -13,7 +24,10 @@ function AppRun(AppConstants, $rootScope, $timeout, Wallet, Alert, $transitions)
     // Check if a wallet is loaded before accessing private states
     $transitions.onStart({ 
         to: (state) => { 
-            return (state.name !== 'app.home') && (state.name !== 'app.login') && (state.name !== 'app.signup') && (state.name !== 'app.faq') && (state.name !== 'app.trezor');
+            for (let i = 0; i < publicStates.length; i++) {
+                if (publicStates[i] === state.name) return false;
+            }
+            return true;
         }
     }, (transition) => {
         if (!Wallet.current) {
