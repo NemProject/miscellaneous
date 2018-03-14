c48b4e2a0c341197ed85389d431941e8d31b7f64
Noriam <correo.suscripciones@outlook.com>
2018-03-14 15:20:17 -0600

    Revert "Revert "Translation to spanish""
    This reverts commit be6e7a1d8eaffb21e9899fecdc95bf0b826560cb.

--- nanowallet/src/app/modules/languages/index.js
+++ nanowallet/src/app/modules/languages/index.js
@@ -25,6 +25,9 @@ app.config(RussianProvider);
 import DutchProvider from './nl';
 app.config(DutchProvider);
 
+import SpanishProvider from './es';
+app.config(SpanishProvider);
+
 // Comment this while developing to see untranslated strings
 app.config(['$translateProvider', function($translateProvider) {
     $translateProvider.fallbackLanguage('en');
