40ac0542af0517e3a7da58b3562422ffc2bbf279
Quantum_Mechanics <Kraken_Labs@outlook.com>
2018-05-28 20:36:49 +0200

    Organize languages list

--- nanowallet/src/app/utils/languages.js
+++ nanowallet/src/app/utils/languages.js
@@ -2,17 +2,11 @@
  * Available languages
  */
 let data =  [{
-    name: "English",
-    key: "en"
-}, {
     name: "Chinese",
     key: "cn"
 }, {
-    name: "Polish",
-    key: "pl"
-}, {
-    name: "Português (Brasil)",
-    key: "ptbr"
+    name: "English",
+    key: "en"
 }, {
     name: "German",
     key: "de"
@@ -20,14 +14,23 @@ let data =  [{
     name: "Japanese",
     key: "jp"
 }, {
-    name: "Русский",
-    key: "ru"
-}, {
     name: "Nederlands",
     key: "nl"
 }, {
+    name: "Polish",
+    key: "pl"
+}, {
+    name: "Português (Brasil)",
+    key: "ptbr"
+}, {
+    name: "Русский",
+    key: "ru"
+}, {
     name: "Spanish",
     key: "es"
+}, {
+    name: "Ukrainian",
+    key: "uk"
 }]
 
 module.exports = {
