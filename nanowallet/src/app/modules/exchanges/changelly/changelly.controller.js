9410fd0eb8f1cc266c10ed8f710085582145a56b
Quantum_Mechanics <QuantumMechanics@users.noreply.github.com>
2019-12-12 19:52:02 +0100

    Fix changelly

--- nanowallet/src/app/modules/exchanges/changelly/changelly.controller.js
+++ nanowallet/src/app/modules/exchanges/changelly/changelly.controller.js
@@ -15,7 +15,7 @@ class ChangellyCtrl {
         //// End dependencies region ////
 
         // Open the Changelly widget in a new window
-        let child = window.open("https://old.changelly.com/widget/v1?auth=email&from=BTC&to=XEM&merchant_id=9bad2685d41a&address="+ this._Wallet.currentAccount.address +"&amount=1&ref_id=9bad2685d41a&color=454545",'1418115287605','width=700,height=500,toolbar=0,menubar=0,location=0,status=1,scrollbars=1,resizable=0,left=0,top=0,noopener=1');
+        let child = window.open("https://widget.changelly.com?currencies=ada,etc,xmr,bch,usdc,trx,neo,eos,xlm,usdt,dash,xrp,ltc,xem,btc,eth&from=btc&to=xem&amount=1&address="+ this._Wallet.currentAccount.address +"&payment_id=&fiat=true&fixedTo=false&theme=jungle&merchant_id=9bad2685d41a",'1418115287605','width=700,height=500,toolbar=0,menubar=0,location=0,status=1,scrollbars=1,resizable=0,left=0,top=0,noopener=1');
         child.opener = null;
     }
 
